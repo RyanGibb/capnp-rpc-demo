7d7dc9f3de3c1de0fb27254f619db9dcda32bc47
Ryan Gibb <ryan@freumh.org>
2023-03-09 12:34:59 +0000

    split into server and client

--- client.ml
+++ client.ml
@@ -24,22 +24,23 @@ let run_client ~stdin ~stdout service =
       in read_to_stdout ()
     );;
 
-let secret_key = `File "secret-key.pem"
-let listen_address = `TCP ("127.0.0.1", 7000)
-
-let start_server ~sw ~stdout ~clock net =
-  let config = Capnp_rpc_unix.Vat_config.create ~secret_key listen_address in
-  let service_id = Capnp_rpc_unix.Vat_config.derived_id config "main" in
-  let restore = Capnp_rpc_net.Restorer.single service_id (Pipe.local ~stdout ~clock) in
-  let vat = Capnp_rpc_unix.serve ~sw ~net ~restore config in
-  Capnp_rpc_unix.Vat.sturdy_uri vat service_id
-
-let () =
+let connect uri =
   Eio_main.run @@ fun env ->
   Eio.Switch.run @@ fun sw ->
-  let uri = start_server ~sw ~stdout:(Eio.Stdenv.stdout env) ~clock:(Eio.Stdenv.clock env) (Eio.Stdenv.net env) in
-  Eio.traceln "Connecting to echo service at: %a" Uri.pp_hum uri;
   let client_vat = Capnp_rpc_unix.client_only_vat ~sw env#net in
   let sr = Capnp_rpc_unix.Vat.import_exn client_vat uri in
-  Capnp_rpc_lwt.Sturdy_ref.with_cap_exn sr (run_client ~stdin:(Eio.Stdenv.stdin env) ~stdout:(Eio.Stdenv.stdout env));
-  raise Exit
+  Capnp_rpc_unix.with_cap_exn sr (run_client ~stdin:(Eio.Stdenv.stdin env) ~stdout:(Eio.Stdenv.stdout env))
+
+open Cmdliner
+
+let connect_addr =
+  let i = Arg.info [] ~docv:"ADDR" ~doc:"Address of server (capnp://...)" in
+  Arg.(required @@ pos 0 (some Capnp_rpc_unix.sturdy_uri) None i)
+
+let connect_cmd =
+  let doc = "run the client" in
+  let info = Cmd.info "connect" ~doc in
+  Cmd.v info Term.(const connect $ connect_addr)
+
+let () =
+  exit (Cmd.eval connect_cmd)
